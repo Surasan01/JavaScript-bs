7db1493dcab84e78aefbfd4bf85098302f18c51a
Surasan01 <141292276+Surasan01@users.noreply.github.com>
2024-03-17 00:06:04 +0700

    up

---
+++ project1/a1.js
@@ -0,0 +1,60 @@
+// console.log("Hello world 111")
+// console.log("Hello world 111")
+// // alert("Hello")
+
+// var name = "Surasan"
+// console.log(name)
+// console.log(name)
+// name = "pop"
+// console.log(name)
+
+// var Firstname = "Surasan"
+// var Lastname = "Phothichak"
+
+// console.log(Firstname)
+// console.log(Lastname)
+        
+// // alert("Hello")
+// var name = "Puiza"
+// // alert(10)
+// var age = 12
+// // alert(typeof 12)
+// console.log(15-5)
+// console.log("Hello world" + " Pui")
+// var age = 2+2*2
+// console.log(age)
+
+// {
+//     var x = 2
+//     let y = 14
+//     alert(y)
+// }
+// console.log(x)
+// alert(y)
+
+// let number = prompt("กรอกเลขที่ต้องการซื้อ : ")
+// console.log(number)
+// console.log("120" + 20)
+
+// let number = prompt("กรอกเลขที่ต้องการซื้อ : ")
+// document.getElementById("mynumber").innerHTML = number
+// console.log(Math.floor(Math.random() * 100)) // สุ่มเลข จาก 0-10
+
+// document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
+
+console.log(1 == 1)
+console.log(2 == 1)
+console.log(4 == "4") // ? True
+
+console.log("6" === "6")
+console.log("Hello" === "Hello")
+console.log(4 === "4")
+
+console.log(4 != "4")
+console.log("4" != "4") // false
+console.log("you have money" != "Hello world")
+
+console.log("4" !== 4) // true
+
+console.log(5 < 10)
+console.log(9 >= 10)
